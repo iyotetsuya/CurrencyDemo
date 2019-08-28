a29a5833a2fc0c684ed0565c9bc9396bea2cba8a
iyotetsuya <iyotetsuya@gmail.com>
2019-08-28 18:03:14 +0900

    remove unused class

--- app/src/main/java/iyotetsuya/currencyconversion/binding/FragmentBindingAdapters.kt
+++
@@ -1,8 +0,0 @@
-//package iyotetsuya.currencyconversion.binding
-//
-//import androidx.fragment.app.Fragment
-//import javax.inject.Inject
-//
-//class FragmentBindingAdapters @Inject constructor(val fragment: Fragment) {
-//
-//}
--- app/src/main/java/iyotetsuya/currencyconversion/ui/common/DataBoundViewHolder.kt
+++
@@ -1,7 +0,0 @@
-//package iyotetsuya.currencyconversion.ui.common
-//
-//import androidx.databinding.ViewDataBinding
-//import androidx.recyclerview.widget.RecyclerView
-//
-//class DataBoundViewHolder<out T : ViewDataBinding> constructor(val binding: T) :
-//        RecyclerView.ViewHolder(binding.root)
